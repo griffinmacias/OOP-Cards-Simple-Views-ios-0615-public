384d0125b0f8b9d88f5718b37865fd007d0e8bc0
griffinmacias <griffinmacias@gmail.com>
2015-06-10 18:29:33 -0400

    Buttons changed Labels Complete

--- OOPCardsSimpleViews/CardSwitcherViewController.h
+++ OOPCardsSimpleViews/CardSwitcherViewController.h
@@ -9,5 +9,14 @@
 #import <UIKit/UIKit.h>
 
 @interface CardSwitcherViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *topLabel;
+@property (weak, nonatomic) IBOutlet UILabel *middleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *bottomLabel;
+
+
+- (IBAction)threeOfSpades:(id)sender;
+- (IBAction)fourOfClubs:(id)sender;
+- (IBAction)eightOfDiamonds:(id)sender;
+- (IBAction)tenOfhearts:(id)sender;
 
 @end
--- OOPCardsSimpleViews/CardSwitcherViewController.m
+++ OOPCardsSimpleViews/CardSwitcherViewController.m
@@ -17,8 +17,33 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
 }
 
 
+
+- (IBAction)threeOfSpades:(id)sender {
+    
+    self.topLabel.text = @"3 ♠️";
+    self.middleLabel.text = @"3 ♠️";
+    self.bottomLabel.text = @"3 ♠️";
+}
+
+- (IBAction)fourOfClubs:(id)sender {
+    self.topLabel.text = @"4 ♣️";
+    self.middleLabel.text = @"4 ♣️";
+    self.bottomLabel.text = @"4 ♣️";
+}
+
+- (IBAction)eightOfDiamonds:(id)sender {
+    self.topLabel.text = @"8 ♦️";
+    self.middleLabel.text = @"8 ♦️";
+    self.bottomLabel.text = @"8 ♦️";
+}
+
+- (IBAction)tenOfhearts:(id)sender {
+    self.topLabel.text = @"10 ♥️";
+    self.middleLabel.text = @"10 ♥️";
+    self.bottomLabel.text = @"10 ♥️";
+}
 @end
